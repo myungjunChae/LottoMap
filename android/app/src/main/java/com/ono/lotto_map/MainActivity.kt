76e21bbe576f8f51670bdd8758f9ad9a10d3962f
MyungjunChae <cmj9597@naver.com>
2020-03-12 02:50:23 +0900

    Init android project

---
+++ android/app/src/main/java/com/ono/lotto_map/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ono.lotto_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
