a2c6532963691f2ab128d62d69cf039fbb97c634
MyungjunChae <cmj9597@naver.com>
2020-05-18 16:41:33 +0900

    Define about StoreInfo for DB with Room

--- android/app/src/main/java/com/ono/lotto_map/data/local/StoreInfoEntity.kt
+++ android/app/src/main/java/com/ono/lotto_map/data/local/StoreInfoEntity.kt
@@ -1,2 +1,17 @@
 package com.ono.lotto_map.data.local
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "store_info_table")
+data class StoreInfoEntity(
+    @PrimaryKey(autoGenerate = true) val store_id: Long,
+    @ColumnInfo(name = "1st") val first_winning: Int,
+    @ColumnInfo(name = "2nd") val second_winning: Int,
+    @ColumnInfo(name = "lat") val lat: Double,
+    @ColumnInfo(name = "lng") val lng: Double,
+    @ColumnInfo(name = "location") val location: String,
+    @ColumnInfo(name = "phone") val phone: String,
+    @ColumnInfo(name = "shop") val shop: String
+)
--- android/app/src/main/java/com/ono/lotto_map/datasource/local/StoreInfoDao.kt
+++ android/app/src/main/java/com/ono/lotto_map/datasource/local/StoreInfoDao.kt
@@ -1,2 +1,28 @@
 package com.ono.lotto_map.datasource.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.ono.lotto_map.data.local.StoreInfoEntity
+
+@Dao
+interface StoreInfoDao{
+    @Insert
+    fun insert(store: StoreInfoEntity)
+
+    @Update
+    fun update(night: StoreInfoEntity)
+
+    @Query("SELECT * FROM store_info_table WHERE store_id = :key")
+    fun get(key: Long): StoreInfoEntity?
+
+    @Query("DELETE from store_info_table")
+    fun clear()
+
+    @Query("SELECT * from store_info_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight() : StoreInfoEntity?
+
+    @Query("SELECT * from store_info_table ORDER BY nightId DESC")
+    fun getAllNight() : List<StoreInfoEntity>
+}
